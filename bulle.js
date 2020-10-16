09a90e9dd179754a7afe4b612332f3ae60c3da1f
Arabhya07092007 <72385709+Arabhya07092007@users.noreply.github.com>
2020-10-16 21:09:21 +0530

    Add files via upload

---
+++ bulle.js
@@ -0,0 +1,17 @@
+
+var lbullet,lwall;
+
+function  hasCollided(lbullet,lwall){
+
+bulletRightEdge = lbullet.x+lwall.width;
+wallLeftEdge =  lwall.x;
+
+if (bulletRightEdge >= wallLeftEdge){
+
+    return true;
+}
+
+return false;
+
+
+}
